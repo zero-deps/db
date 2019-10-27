c583d1ed5c60148fc09c169b6bcf72ec3af0410c
Andrii Nemchenko <a.nemchenko@icloud.com>
2019-10-27 15:57:41 +0200

    open, close

--- src/main/scala/db.scala
+++ src/main/scala/db.scala
@@ -1,4 +1,21 @@
 package zd.db
 
+import java.io.{File, RandomAccessFile}
+import java.nio.file.{Files, Paths}
+
 class Db {
+  var main: RandomAccessFile = null
+  def open(): Unit = {
+    Files.createDirectories(Paths.get("dat"))
+    main = new RandomAccessFile("dat/main.db", "rw")
+  }
+  def close(): Unit = {
+    if (main != null) main.close()
+  }
+}
+
+object Main extends App {
+  val db = new Db()
+  db.open()
+  db.close()
 }
