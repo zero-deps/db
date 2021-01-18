cd6957fd820cab0bb373fb1db74f4e3bb1ccf648
Andrii Nemchenko <a.nemchenko@icloud.com>
2021-01-18 02:49:16 +0200

    entries

--- build.sbt
+++ build.sbt
@@ -3,7 +3,7 @@ version := zero.git.version()
 
 libraryDependencies ++= Seq(
   "org.rocksdb" % "rocksdbjni" % "6.15.2"
-, "dev.zio" %% "zio" % "1.0.3+130-a21e83b8-SNAPSHOT"
+, "dev.zio" %% "zio-streams" % "1.0.3+130-a21e83b8-SNAPSHOT"
 )
 resolvers += Resolver.sonatypeRepo("snapshots")
 
--- src/main/scala/store.scala
+++ src/main/scala/store.scala
@@ -1,6 +1,7 @@
 package db
 
-import zio._, duration._, IO.{effect, effectTotal}
+import zio._, duration._, stream._
+import zio.IO.{effect, effectTotal}
 import zero.ext._, option._
 import zd.proto.{Bytes=>_,_}, api.MessageCodec, macrosapi._
 import org.rocksdb.{util=>_,_}
@@ -8,17 +9,16 @@ import java.util.Arrays
 
 object Store {
   trait Service {
-    def add(fid: Fid, dat: Dat): RIO[ZEnv, Eid]
-    // def put(fid: Fid, eid: Eid, dat: Dat): ZIO[ZEnv, Throwable | NotExists, Eid]
-    def getOrFail(fid: Fid, eid: Eid): ZIO[ZEnv, Throwable | NotExists, Dat]
-    def get(fid: Fid, eid: Eid): RIO[ZEnv, Option[Dat]]
-    // def entries(fid: Fid): ZStream[ZEnv, Throwable, Eid]
-    // def all(fid: Fid): ZStream[ZEnv, Throwable, Dat] =
-    //   entries(fid).mapM(get(fid, _))
+    def add(fid: Fid, dat: Dat): RIO[Any, Eid] //todo: UIO
+    // def getOrFail(fid: Fid, eid: Eid): ZIO[Any, Throwable | NotExists, Dat]
+    def get(fid: Fid, eid: Eid): RIO[Any, Option[Dat]]
+    // def entries(fid: Fid): ZStream[Any, Throwable, Eid]
+    // def all(fid: Fid): ZStream[Any, Throwable, Dat] =
+    //   entries(fid).mapM(getOrFail(fid, _))
     def close(): UIO[Unit]
   }
 
-  def live(dir: String): ZLayer[ZEnv, Throwable, Store] =
+  def live(dir: String): ZLayer[Any, Throwable, Store] =
     ZLayer.fromAcquireRelease {
       for {
         _  <- effect(RocksDB.loadLibrary())
@@ -35,19 +35,19 @@ object Store {
               } yield y
       } yield new Service {
 
-        def get(fid: Fid, eid: Eid): RIO[ZEnv, Option[Dat]] =
+        def get(fid: Fid, eid: Eid): RIO[Any, Option[Dat]] =
           for {
             k  <- Tuple3(fid, eid, "dat").encode
             x  <- db.eff_get(k)
           } yield x
 
-        def getOrFail(fid: Fid, eid: Eid): ZIO[ZEnv, Throwable | NotExists, Dat] =
-          for {
-            x  <- get(fid, eid)
-            x1 <- ZIO.getOrFailWith(NotExists)(x)
-          } yield x1
+        // def getOrFail(fid: Fid, eid: Eid): ZIO[Any, Throwable | NotExists, Dat] =
+        //   for {
+        //     x  <- get(fid, eid)
+        //     x1 <- ZIO.getOrFailWith(NotExists)(x)
+        //   } yield x1
 
-        def add(fid: Fid, dat: Dat): RIO[ZEnv, Eid] =
+        def add(fid: Fid, dat: Dat): RIO[Any, Eid] =
           for {
             tx <- db.eff_tx(wo)
             l  <- tx.eff_get(fid, ro)
@@ -65,11 +65,18 @@ object Store {
             _  <- tx.eff_commit()
           } yield l1
 
-        // def put(fid: Fid, eid: Eid, dat: Dat): ZIO[ZEnv, Throwable | NotExists, Eid] =
-        //   for {
-        //     tx <- effect(db.beginTransaction(wo))
-        //     _  <- IO.
-        //   }
+        private def entries(fid: Fid): ZStream[Any, Throwable, Dat] =
+          ZStream.fromEffect(db.eff_get(fid)).flatMap{
+            case None => Stream.empty
+            case Some(x) =>
+              Stream.unfoldM(x){ eid =>
+                for {
+                  k <- Tuple2(fid, eid).encode
+                  v <- IO.require(new Exception("broken"))(db.eff_get(k))
+                  s <- v.decode[Tuple1[Option[Eid]]]
+                } yield s._1.map(s=>(eid,s))
+              }
+          }
 
         private given MessageCodec[Tuple1[Option[Eid]]] = caseCodecIdx
         private given MessageCodec[Tuple2[Fid,Eid]] = caseCodecIdx
@@ -80,22 +87,17 @@ object Store {
     }(_.close())
 }
 
-type ZStore = Store with ZEnv
-
-// def all(fid: Fid): ZStream[ZStore, Throwable, Dat] =
+// def all(fid: Fid): ZStream[Store, Throwable, Dat] =
 //   ZIO.accessM(_.get.all(fid))
 
-def get(fid: Fid, id: Eid): RIO[ZStore, Option[Dat]] =
+def get(fid: Fid, id: Eid): RIO[Store, Option[Dat]] =
   ZIO.accessM(_.get.get(fid, id))
 
-def getOrFail(fid: Fid, id: Eid): ZIO[ZStore, Throwable | NotExists, Dat] =
-  ZIO.accessM(_.get.getOrFail(fid, id))
+// def getOrFail(fid: Fid, id: Eid): ZIO[Store, Throwable | NotExists, Dat] =
+//   ZIO.accessM(_.get.getOrFail(fid, id))
 
-def add(fid: Fid, dat: Dat): RIO[ZStore, Eid] =
+def add(fid: Fid, dat: Dat): RIO[Store, Eid] =
   ZIO.accessM(_.get.add(fid, dat))
-
-// def put(fid: Fid, id: Eid, dat: Dat): ZIO[ZStore, Throwable | NotExists, Unit] =
-//   ZIO.accessM(_.get.put(fid, id, dat))
 
 type Store = Has[Store.Service]
 type Bytes = Array[Byte]
@@ -151,10 +153,10 @@ extension (tx: Transaction)
   def eff_commit(): Task[Unit] =
     effect(tx.commit())
 
-extension[A] (xs: Array[Byte])
+extension [A](xs: Array[Byte])
   def decode(using c: MessageCodec[A]): Task[A] =
     effect(api.decode(xs))
 
-extension[A] (x: A)
+extension [A](x: A)
   def encode(using c: MessageCodec[A]): UIO[Bytes] =
     effectTotal(api.encode(x))
